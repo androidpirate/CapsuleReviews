2df83186398abd0474e694f0eca00a148ef766d3
androipirate <emrekalaycidesign@gmail.com>
2020-07-31 13:57:53 -0400

    New feat - Add bottom navigation to main activity.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/MainActivity.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.github.androidpirate.capsulereviews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.github.androidpirate.capsulereviews.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navController = Navigation.findNavController(this, R.id.navHostFragment)
+        bottomNavigationBar.setupWithNavController(navController)
     }
 }
