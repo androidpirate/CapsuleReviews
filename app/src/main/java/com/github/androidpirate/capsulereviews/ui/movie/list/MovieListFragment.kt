b4b075a037c2f71e1f389f3f44d1aa7baeb3d54c
android_pirate <emrekalaycidesign@gmail.com>
2020-08-09 23:36:55 -0400

    Refactor - Moved implementation of views into a separate function.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/movie/list/MovieListFragment.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/movie/list/MovieListFragment.kt
@@ -1,6 +1,5 @@
 package com.github.androidpirate.capsulereviews.ui.movie.list
 
-import android.graphics.Movie
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.github.androidpirate.capsulereviews.R
 import com.github.androidpirate.capsulereviews.data.api.MovieDbService
-import com.github.androidpirate.capsulereviews.data.response.movie.MovieResponse
 import com.github.androidpirate.capsulereviews.data.response.movies.MoviesListItem
 import com.github.androidpirate.capsulereviews.ui.adapter.ListItemAdapter
 import com.github.androidpirate.capsulereviews.util.ItemClickListener
@@ -21,14 +19,15 @@ import kotlinx.coroutines.withContext
 
 class MovieListFragment : Fragment(), ItemClickListener {
     private lateinit var popularMoviesAdapter: ListItemAdapter<MoviesListItem>
+    private lateinit var popularMovies: List<MoviesListItem>
     private lateinit var topRatedMoviesAdapter: ListItemAdapter<MoviesListItem>
+    private lateinit var topRatedMovies: List<MoviesListItem>
     private lateinit var nowPlayingMoviesAdapter: ListItemAdapter<MoviesListItem>
+    private lateinit var nowPlayingMovies: List<MoviesListItem>
     private lateinit var upcomingMoviesAdapter: ListItemAdapter<MoviesListItem>
+    private lateinit var upcomingMovies: List<MoviesListItem>
     private lateinit var trendingMoviesAdapter: ListItemAdapter<MoviesListItem>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var trendingMovies: List<MoviesListItem>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,45 +37,56 @@ class MovieListFragment : Fragment(), ItemClickListener {
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        popularMoviesAdapter = ListItemAdapter(MovieListFragment::class.simpleName, this)
-        topRatedMoviesAdapter = ListItemAdapter(MovieListFragment::class.simpleName, this)
-        nowPlayingMoviesAdapter = ListItemAdapter(MovieListFragment::class.simpleName, this)
-        upcomingMoviesAdapter = ListItemAdapter(MovieListFragment::class.simpleName, this)
-        trendingMoviesAdapter = ListItemAdapter(MovieListFragment::class.simpleName, this)
+        popularMoviesAdapter = ListItemAdapter(
+            MovieListFragment::class.simpleName, this)
+        topRatedMoviesAdapter = ListItemAdapter(
+            MovieListFragment::class.simpleName, this)
+        nowPlayingMoviesAdapter = ListItemAdapter(
+            MovieListFragment::class.simpleName, this)
+        upcomingMoviesAdapter = ListItemAdapter(
+            MovieListFragment::class.simpleName, this)
+        trendingMoviesAdapter = ListItemAdapter(
+            MovieListFragment::class.simpleName, this)
         val apiService = MovieDbService()
         GlobalScope.launch(Dispatchers.Main ) {
 
-            popularMoviesAdapter.submitList(
-                withContext(Dispatchers.IO) { apiService.getPopularMovies().moviesListItems }
-            )
-            topRatedMoviesAdapter.submitList(
-                withContext(Dispatchers.IO) { apiService.getTopRatedMovies().moviesListItems }
-            )
-            nowPlayingMoviesAdapter.submitList(
-                withContext(Dispatchers.IO) { apiService.getNowPlayingMovies().moviesListItems }
-            )
-            upcomingMoviesAdapter.submitList(
-                withContext(Dispatchers.IO) { apiService.getUpcomingMovies().moviesListItems }
-            )
-            trendingMoviesAdapter.submitList(
-                withContext(Dispatchers.IO) {apiService.getTrendingMovies().moviesListItems }
-            )
+            withContext(Dispatchers.IO) {
+                popularMovies = apiService.getPopularMovies().moviesListItems
+            }
+            withContext(Dispatchers.IO) {
+                topRatedMovies = apiService.getTopRatedMovies().moviesListItems
+            }
+            withContext(Dispatchers.IO) {
+                nowPlayingMovies = apiService.getNowPlayingMovies().moviesListItems
+            }
+            withContext(Dispatchers.IO) {
+                upcomingMovies = apiService.getUpcomingMovies().moviesListItems
+            }
+            withContext(Dispatchers.IO) {
+                trendingMovies = apiService.getTrendingMovies().moviesListItems
+            }
+            setupViews()
         }
+    }
+
+    private fun setupViews() {
+        popularMoviesAdapter.submitList(popularMovies)
         rvPopular.adapter = popularMoviesAdapter
+        topRatedMoviesAdapter.submitList(topRatedMovies)
         rvTopRated.adapter = topRatedMoviesAdapter
+        nowPlayingMoviesAdapter.submitList(nowPlayingMovies)
         rvNowPlaying.adapter = nowPlayingMoviesAdapter
+        upcomingMoviesAdapter.submitList(upcomingMovies)
         rvUpcoming.adapter = upcomingMoviesAdapter
+        trendingMoviesAdapter.submitList(trendingMovies)
         rvTrending.adapter = trendingMoviesAdapter
     }
 
     override fun <T> onItemClick(item: T) {
-        val action = MovieListFragmentDirections.actionMovieListToDetail((item as MoviesListItem).id)
+        val action = MovieListFragmentDirections
+            .actionMovieListToDetail((item as MoviesListItem).id)
         findNavController().navigate(action)
     }
 }
