cd5a57cdc4d798d0104cf97cda8c316612fb2010
android_pirate <emrekalaycidesign@gmail.com>
2020-08-22 00:29:26 -0400

    Refactor - Update movie list item type.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/adapter/ListItemAdapter.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/adapter/ListItemAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.github.androidpirate.capsulereviews.R
+import com.github.androidpirate.capsulereviews.data.db.entity.DBMovie
 import com.github.androidpirate.capsulereviews.data.network.response.movies.NetworkMoviesListItem
 import com.github.androidpirate.capsulereviews.data.network.response.tvShows.NetworkTvShowsListItem
 import com.github.androidpirate.capsulereviews.util.ItemClickListener
@@ -97,7 +98,7 @@ class ListItemAdapter<T>(private val fragmentType: String?, private val clickLis
         fun onBindItem(item: T) {
             itemView.setOnClickListener { clickListener.onItemClick(item) }
             when(fragmentType) {
-                MOVIE_LIST -> setItemThumbnail((item as NetworkMoviesListItem).posterPath)
+                MOVIE_LIST -> setItemThumbnail((item as DBMovie).posterPath)
                 MOVIE_DETAIL -> setSimilarItemThumbnail((item as NetworkMoviesListItem).posterPath)
                 TV_LIST -> setItemThumbnail((item as NetworkTvShowsListItem).posterPath)
                 TV_DETAIL -> setSimilarItemThumbnail((item as NetworkTvShowsListItem).posterPath)
