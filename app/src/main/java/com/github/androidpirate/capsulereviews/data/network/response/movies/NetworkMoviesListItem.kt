87407ee7101b2cd0e77b1d84e75dfad27aeee83a
android_pirate <emrekalaycidesign@gmail.com>
2020-08-22 00:25:00 -0400

    Refactor - Implement extension functions to convert DTO to entity.

--- app/src/main/java/com/github/androidpirate/capsulereviews/data/network/response/movies/NetworkMoviesListItem.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/data/network/response/movies/NetworkMoviesListItem.kt
@@ -2,9 +2,10 @@ package com.github.androidpirate.capsulereviews.data.network.response.movies
 
 
 import androidx.annotation.Nullable
+import com.github.androidpirate.capsulereviews.data.db.entity.DBMovie
 import com.google.gson.annotations.SerializedName
 
-data class NetworkMoviesListItem(
+class NetworkMoviesListItem(
     @SerializedName("id")
     val id: Int,
     @SerializedName("title")
@@ -16,4 +17,49 @@ data class NetworkMoviesListItem(
     val video: Boolean,
     @SerializedName("vote_average")
     val voteAverage: Double
-)
+) {
+    fun toPopularMovie() = DBMovie(
+        this.id,
+        this.title,
+        this.posterPath,
+        this.video,
+        this.voteAverage,
+        popular = true
+    )
+
+    fun toTopRated() = DBMovie(
+        this.id,
+        this.title,
+        this.posterPath,
+        this.video,
+        this.voteAverage,
+        topRated = true
+    )
+
+    fun toNowPlaying() = DBMovie(
+        this.id,
+        this.title,
+        this.posterPath,
+        this.video,
+        this.voteAverage,
+        nowPlaying = true
+    )
+
+    fun toUpcoming() = DBMovie(
+        this.id,
+        this.title,
+        this.posterPath,
+        this.video,
+        this.voteAverage,
+        upcoming = true
+    )
+
+    fun toTrending() = DBMovie(
+        this.id,
+        this.title,
+        this.posterPath,
+        this.video,
+        this.voteAverage,
+        trending = true
+    )
+}
