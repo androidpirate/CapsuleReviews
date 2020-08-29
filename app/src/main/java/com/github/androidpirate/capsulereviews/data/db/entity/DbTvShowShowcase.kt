e910ef68d411cf76eda1da802d6867e8c7b73d0c
android_pirate <emrekalaycidesign@gmail.com>
2020-08-28 23:44:28 -0400

    New feat - Implement entity class for tv showcase.

---
+++ app/src/main/java/com/github/androidpirate/capsulereviews/data/db/entity/DbTvShowShowcase.kt
@@ -0,0 +1,20 @@
+package com.github.androidpirate.capsulereviews.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tv_showcase")
+data class DbTvShowShowcase(
+    val tvShowId: Int,
+    val title: String,
+    val posterPath: String
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = ID
+    var videoKey: String = EMPTY_VIDEO_KEY
+
+    companion object {
+        const val ID = 0
+        const val EMPTY_VIDEO_KEY = ""
+    }
+}
