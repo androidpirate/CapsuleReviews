7c82c80d467cedfa04288287c29228e46e92bc76
android_pirate <emrekalaycidesign@gmail.com>
2020-08-28 22:49:53 -0400

    New feat - Implement observing data from the database using view model.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/tv/list/TvListFragment.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/tv/list/TvListFragment.kt
@@ -8,16 +8,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.github.androidpirate.capsulereviews.BuildConfig
 import com.github.androidpirate.capsulereviews.R
+import com.github.androidpirate.capsulereviews.data.db.entity.DBTvShow
 import com.github.androidpirate.capsulereviews.data.network.api.MovieDbService
 import com.github.androidpirate.capsulereviews.data.network.response.tvShows.NetworkTvShowsListItem
 import com.github.androidpirate.capsulereviews.data.network.response.videos.NetworkVideosListItem
 import com.github.androidpirate.capsulereviews.ui.adapter.ListItemAdapter
 import com.github.androidpirate.capsulereviews.util.ItemClickListener
+import com.github.androidpirate.capsulereviews.viewmodel.TvShowListViewModel
+import com.github.androidpirate.capsulereviews.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.fragment_movie_list.*
 import kotlinx.android.synthetic.main.fragment_tv_list.*
+import kotlinx.android.synthetic.main.fragment_tv_list.container
+import kotlinx.android.synthetic.main.fragment_tv_list.loadingScreen
+import kotlinx.android.synthetic.main.fragment_tv_list.rvPopular
+import kotlinx.android.synthetic.main.fragment_tv_list.rvTopRated
+import kotlinx.android.synthetic.main.fragment_tv_list.rvTrending
 import kotlinx.android.synthetic.main.tv_showcase.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -26,15 +37,13 @@ import kotlinx.coroutines.withContext
 import kotlin.random.Random
 
 class TvListFragment : Fragment(), ItemClickListener{
-    private lateinit var popularShowsAdapterNetwork: ListItemAdapter<NetworkTvShowsListItem>
-    private lateinit var popularShows: List<NetworkTvShowsListItem>
-    private lateinit var topRatedShowsAdapterNetwork: ListItemAdapter<NetworkTvShowsListItem>
-    private lateinit var topRatedShows: List<NetworkTvShowsListItem>
-    private lateinit var trendingShowsAdapterNetwork: ListItemAdapter<NetworkTvShowsListItem>
-    private lateinit var trendingShows: List<NetworkTvShowsListItem>
+    private lateinit var popularShowsAdapter: ListItemAdapter<DBTvShow>
+    private lateinit var topRatedShowsAdapter: ListItemAdapter<DBTvShow>
+    private lateinit var trendingShowsAdapter: ListItemAdapter<DBTvShow>
     private lateinit var showCaseNetworkTvShow: NetworkTvShowsListItem
     private lateinit var showCaseVideos: List<NetworkVideosListItem>
     private var videoKey: String ?= null
+    private lateinit var viewModel: TvShowListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,42 +58,51 @@ class TvListFragment : Fragment(), ItemClickListener{
         return inflater.inflate(R.layout.fragment_tv_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+        displayLoadingScreen()
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val apiService = MovieDbService()
-        GlobalScope.launch(Dispatchers.Main) {
-            container.visibility = View.GONE
-            loadingScreen.visibility = View.VISIBLE
+        val factory = ViewModelFactory(requireActivity().application)
+        viewModel = ViewModelProvider(this, factory).get(TvShowListViewModel::class.java)
+        viewModel.popularTvShows.observe(viewLifecycleOwner, Observer {
+            popularShowsAdapter.submitList(it)
+        })
+        viewModel.topRatedTvShows.observe(viewLifecycleOwner, Observer {
+            topRatedShowsAdapter.submitList(it)
+        })
+        viewModel.trendingTvShows.observe(viewLifecycleOwner, Observer {
+            trendingShowsAdapter.submitList(it)
+        })
+    }
 
-            withContext(Dispatchers.IO) {
-                popularShows = apiService.getPopularTvShows().networkTvShowsListItems
-            }
-            withContext(Dispatchers.IO) {
-                topRatedShows = apiService.getTopRatedTvShows().networkTvShowsListItems
-            }
-            withContext(Dispatchers.IO) {
-               trendingShows = apiService.getTrendingTvShows().networkTvShowsListItems
-                val randomItemNo = Random.nextInt(0 , 19)
-                showCaseNetworkTvShow = trendingShows[randomItemNo]
-            }
-            withContext(Dispatchers.IO) {
-                showCaseVideos = apiService.getTvShowVideos(showCaseNetworkTvShow.id).networkVideosListItems
-                for(video in showCaseVideos) {
-                    if(video.site == "YouTube" && video.type == "Trailer") {
-                        videoKey = video.key
-                        break
-                    }
-                }
-            }
-            setShowCaseTvShow()
-            setupViews()
-        }
+    override fun onStart() {
+        super.onStart()
+//        setShowCaseTvShow()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        displayContainerScreen()
+    }
+
+    private fun displayLoadingScreen() {
+        container.visibility = View.GONE
+        loadingScreen.visibility = View.VISIBLE
+    }
+
+    private fun displayContainerScreen() {
+        loadingScreen.visibility = View.GONE
+        container.visibility = View.VISIBLE
     }
 
     private fun setupAdapters() {
-        popularShowsAdapterNetwork = ListItemAdapter(TvListFragment::class.simpleName, this)
-        topRatedShowsAdapterNetwork = ListItemAdapter(TvListFragment::class.simpleName, this)
-        trendingShowsAdapterNetwork = ListItemAdapter(TvListFragment::class.simpleName, this)
+        popularShowsAdapter = ListItemAdapter(TvListFragment::class.simpleName, this)
+        topRatedShowsAdapter = ListItemAdapter(TvListFragment::class.simpleName, this)
+        trendingShowsAdapter = ListItemAdapter(TvListFragment::class.simpleName, this)
     }
 
     private fun setShowCaseTvShow() {
@@ -119,14 +137,9 @@ class TvListFragment : Fragment(), ItemClickListener{
     }
 
     private fun setupViews() {
-        popularShowsAdapterNetwork.submitList(popularShows)
-        topRatedShowsAdapterNetwork.submitList(topRatedShows)
-        trendingShowsAdapterNetwork.submitList(trendingShows)
-        rvPopular.adapter = popularShowsAdapterNetwork
-        rvTopRated.adapter = topRatedShowsAdapterNetwork
-        rvTrending.adapter = trendingShowsAdapterNetwork
-        loadingScreen.visibility = View.GONE
-        container.visibility = View.VISIBLE
+        rvPopular.adapter = popularShowsAdapter
+        rvTopRated.adapter = topRatedShowsAdapter
+        rvTrending.adapter = trendingShowsAdapter
     }
 
     override fun <T> onItemClick(item: T) {
