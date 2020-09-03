108c88540712f605255fa66b541369d7ec7263d1
android_pirate <emrekalaycidesign@gmail.com>
2020-09-03 13:17:00 -0400

    New feat - Implement a new class for global constants.

---
+++ app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/Constants.kt
@@ -0,0 +1,11 @@
+package com.github.androidpirate.capsulereviews.util.internal
+
+public final class Constants {
+    companion object {
+        const val ADAPTER_UNKNOWN_FRAGMENT_MESSAGE = "Unknown fragment type"
+        const val ADAPTER_UNKNOWN_VIEW_TYPE_MESSAGE = "Unknown view type"
+        const val ADAPTER_POSTER_WIDTH = "w185/"
+        const val SHOWCASE_POSTER_WIDTH = "w342/"
+        const val EMPTY_VIDEO_KEY = ""
+    }
+}
