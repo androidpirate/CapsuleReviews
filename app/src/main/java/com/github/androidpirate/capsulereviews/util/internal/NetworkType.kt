a00c45cd8079def21d426b63fbd5f7f817ca3bf8
android_pirate <emrekalaycidesign@gmail.com>
2020-09-05 14:56:53 -0400

    New feat - Add an enum class for network types.

---
+++ app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/NetworkType.kt
@@ -0,0 +1,12 @@
+package com.github.androidpirate.capsulereviews.util.internal
+
+enum class NetworkType(val id: Int) {
+    NETFLIX(213),
+    HULU(453),
+    DISNEY_PLUS(2739),
+    AMAZON_PRIME(1024),
+    HBO_NOW(49),
+    HBO_MAX(3186),
+    APPLE_TV(2552),
+    PEACOCK(3353)
+}
