b1d8e83fdfb408fd99c05df5c3a6afb4c50135df
android_pirate <emrekalaycidesign@gmail.com>
2020-09-14 12:26:07 -0400

    Refactor - Update paged movies data source and factory to handle sort and genre types.

--- app/src/main/java/com/github/androidpirate/capsulereviews/data/datasource/PagedMoviesDataSource.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/data/datasource/PagedMoviesDataSource.kt
@@ -4,9 +4,12 @@ import androidx.paging.PageKeyedDataSource
 import com.github.androidpirate.capsulereviews.data.network.api.MovieDbService
 import com.github.androidpirate.capsulereviews.data.network.response.movies.NetworkMoviesListItem
 import com.github.androidpirate.capsulereviews.data.network.response.movies.NetworkMoviesResponse
-import com.github.androidpirate.capsulereviews.util.internal.GenreType
 import com.github.androidpirate.capsulereviews.util.internal.SortType
-import com.github.androidpirate.capsulereviews.util.internal.SortType.*
+import com.github.androidpirate.capsulereviews.util.internal.Constants
+import com.github.androidpirate.capsulereviews.util.internal.GenreType
+import com.github.androidpirate.capsulereviews.util.internal.GenericSortType
+import com.github.androidpirate.capsulereviews.util.internal.GenericSortType.*
+import com.github.androidpirate.capsulereviews.util.internal.GenreType.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,29 +18,28 @@ import kotlinx.coroutines.withContext
 class PagedMoviesDataSource(
     private val api: MovieDbService,
     private val scope: CoroutineScope,
+    private val genericSort: GenericSortType,
     private val sort: SortType,
     private val genre: GenreType)
     : PageKeyedDataSource<Int, NetworkMoviesListItem>() {
 
     private lateinit var response: NetworkMoviesResponse
-    private var page = FIRST_PAGE
+    private var page = Constants.FIRST_PAGE
 
     override fun loadInitial(
         params: LoadInitialParams<Int>,
         callback: LoadInitialCallback<Int, NetworkMoviesListItem>) {
         scope.launch {
             withContext(Dispatchers.IO) {
-                response = when (sort) {
-                    POPULAR -> getPagedPopularMovies(page)
-                    TOP_RATED -> getPagedTopRatedMovies(page)
-                    NOW_PLAYING -> getPagedNowPlayingMovies(page)
-                    UPCOMING -> getPagedUpcomingMovies(page)
-                    TRENDING -> getPagedTrendingMovies(page)
+                response = if(genre == ALL) {
+                    getPagedMoviesWithGenericSort(page, genericSort)
+                } else {
+                    getPagedMoviesWithGenre(page, genre, sort)
                 }
                 callback.onResult(
                     response.networkMoviesListItems,
                     null,
-                    page + INITIAL_LOAD_INCREMENT)
+                    page + Constants.PAGE_LOAD_INCREMENT)
             }
         }
     }
@@ -45,17 +47,17 @@ class PagedMoviesDataSource(
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, NetworkMoviesListItem>) {
         scope.launch {
             withContext(Dispatchers.IO) {
-                response = when(sort) {
-                    POPULAR -> api.getPagedPopularMovies(params.key)
-                    TOP_RATED -> api.getPagedTopRatedMovies(params.key)
-                    NOW_PLAYING -> api.getPagedNowPlayingMovies(params.key)
-                    UPCOMING -> api.getPagedUpcomingMovies(params.key)
-                    TRENDING -> api.getPagedTrendingMovies(params.key)
-                }
+                response =
+                    if(genre == ALL) {
+                        getPagedMoviesWithGenericSort(params.key, genericSort)
+                    } else {
+                        getPagedMoviesWithGenre(params.key, genre, sort)
+                    }
+
                 if (response.totalPages >= params.key) {
                     callback.onResult(
                         response.networkMoviesListItems,
-                        params.key + 1)
+                        params.key + Constants.PAGE_LOAD_INCREMENT)
                 }
             }
         }
@@ -64,28 +66,17 @@ class PagedMoviesDataSource(
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, NetworkMoviesListItem>) {
     }
 
-    private suspend fun getPagedPopularMovies(page: Int): NetworkMoviesResponse {
-        return api.getPagedPopularMovies(page)
+    private suspend fun getPagedMoviesWithGenericSort(page: Int, genericSort: GenericSortType): NetworkMoviesResponse {
+        return when(genericSort) {
+            POPULAR -> api.getPagedPopularMovies(page)
+            TOP_RATED -> api.getPagedTopRatedMovies(page)
+            NOW_PLAYING -> api.getPagedNowPlayingMovies(page)
+            UPCOMING -> api.getPagedUpcomingMovies(page)
+            TRENDING -> api.getPagedTrendingMovies(page)
+        }
     }
 
-    private suspend fun getPagedTopRatedMovies(page: Int): NetworkMoviesResponse {
-        return api.getPagedTopRatedMovies(page)
-    }
-
-    private suspend fun getPagedNowPlayingMovies(page: Int): NetworkMoviesResponse {
-        return api.getPagedNowPlayingMovies(page)
-    }
-
-    private suspend fun getPagedUpcomingMovies(page: Int): NetworkMoviesResponse {
-        return api.getPagedUpcomingMovies(page)
-    }
-
-    private suspend fun getPagedTrendingMovies(page: Int): NetworkMoviesResponse {
-        return api.getPagedTrendingMovies(page)
-    }
-
-    companion object {
-        const val FIRST_PAGE = 1
-        const val INITIAL_LOAD_INCREMENT = 1
+    private suspend fun getPagedMoviesWithGenre(page: Int, genre: GenreType, sort: SortType): NetworkMoviesResponse {
+       return api.getPagedMoviesWithGenre(page, genre.id, sort.queryParameter)
     }
 }
--- app/src/main/java/com/github/androidpirate/capsulereviews/data/datasource/PagedMoviesDataSourceFactory.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/data/datasource/PagedMoviesDataSourceFactory.kt
@@ -3,18 +3,20 @@ package com.github.androidpirate.capsulereviews.data.datasource
 import androidx.paging.DataSource
 import com.github.androidpirate.capsulereviews.data.network.api.MovieDbService
 import com.github.androidpirate.capsulereviews.data.network.response.movies.NetworkMoviesListItem
-import com.github.androidpirate.capsulereviews.util.internal.GenreType
 import com.github.androidpirate.capsulereviews.util.internal.SortType
+import com.github.androidpirate.capsulereviews.util.internal.GenreType
+import com.github.androidpirate.capsulereviews.util.internal.GenericSortType
 import kotlinx.coroutines.CoroutineScope
 
 class PagedMoviesDataSourceFactory(
     private val api: MovieDbService,
     private val scope: CoroutineScope,
+    private val genericSort: GenericSortType,
     private val sort: SortType,
     private val genre: GenreType)
     : DataSource.Factory<Int, NetworkMoviesListItem>() {
 
     override fun create(): DataSource<Int, NetworkMoviesListItem> {
-        return PagedMoviesDataSource(api, scope, sort, genre)
+        return PagedMoviesDataSource(api, scope, genericSort, sort, genre)
     }
 }
