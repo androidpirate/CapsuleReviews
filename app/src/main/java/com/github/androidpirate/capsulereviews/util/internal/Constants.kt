5017e2aa1eb24c22e21cc2c2c22bde9f650df0f4
android_pirate <emrekalaycidesign@gmail.com>
2020-09-14 12:32:18 -0400

    Refactor - Update constants.

--- app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/Constants.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/Constants.kt
@@ -7,5 +7,19 @@ public final class Constants {
         const val ADAPTER_POSTER_WIDTH = "w185/"
         const val SHOWCASE_POSTER_WIDTH = "w342/"
         const val EMPTY_VIDEO_KEY = ""
+        const val FIRST_PAGE = 1
+        const val PAGE_LOAD_INCREMENT = 1
+        const val PAGE_SIZE = 20
+        const val MOVIE_POPULAR_TITLE = "Popular Movies"
+        const val MOVIE_TOP_RATED_TITLE = "Top Rated Movies"
+        const val MOVIE_NOW_PLAYING_TITLE = "Now Playing Movies"
+        const val MOVIE_UPCOMING_TITLE = "Upcoming Movies"
+        const val MOVIE_TRENDING_TITLE = "Trending Movies"
+        const val TV_POPULAR_TITLE = "Popular TV Shows"
+        const val TV_TOP_RATED_TITLE = "Top Rated TV shows"
+        const val TV_TRENDING_TITLE = "Trending TV Shows"
+        const val TV_TRENDING_NETFLIX_TITLE = "Trending TV Shows on Netflix"
+        const val TV_TRENDING_HULU_TITLE = "Trending TV Shows on Hulu"
+        const val TV_TRENDING_DISNEY_TITLE = "Trending TV Shows on Disney Plus"
     }
 }
