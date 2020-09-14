12a6253cda72c6e8dd892ab82ea48c6a8cdadd41
android_pirate <emrekalaycidesign@gmail.com>
2020-09-14 12:22:28 -0400

    New feat - Add new config constants for sorting.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/adapter/list/ItemClickListener.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/adapter/list/ItemClickListener.kt
@@ -1,8 +1,8 @@
 package com.github.androidpirate.capsulereviews.ui.adapter.list
 
-import com.github.androidpirate.capsulereviews.util.internal.SortType
+import com.github.androidpirate.capsulereviews.util.internal.GenericSortType
 
 interface ItemClickListener {
 
-    fun<T> onItemClick(item: T, isLast: Boolean, sort: SortType)
+    fun<T> onItemClick(item: T, isLast: Boolean, genericSort: GenericSortType)
 }
