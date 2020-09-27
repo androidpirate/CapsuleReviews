4a8c746030f6685900f1850e6bc3c6005fafbcd1
android_pirate <emrekalaycidesign@gmail.com>
2020-09-27 13:31:12 -0400

    Refactor - Add new constants for favorites fragments.

--- app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/Constants.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/util/internal/Constants.kt
@@ -2,14 +2,20 @@ package com.github.androidpirate.capsulereviews.util.internal
 
 public final class Constants {
     companion object {
+        // General constants
+        const val COMMA_SEPARATOR = ","
+        // Adapter constants
         const val ADAPTER_UNKNOWN_FRAGMENT_MESSAGE = "Unknown fragment type"
         const val ADAPTER_UNKNOWN_VIEW_TYPE_MESSAGE = "Unknown view type"
         const val ADAPTER_POSTER_WIDTH = "w185/"
+        // Item constants
         const val SHOWCASE_POSTER_WIDTH = "w342/"
         const val EMPTY_VIDEO_KEY = ""
+        // Data source constants
         const val FIRST_PAGE = 1
         const val PAGE_LOAD_INCREMENT = 1
         const val PAGE_SIZE = 20
+        // Toolbar title constants
         const val MOVIE_POPULAR_TITLE = "Popular Movies"
         const val MOVIE_TOP_RATED_TITLE = "Top Rated Movies"
         const val MOVIE_NOW_PLAYING_TITLE = "Now Playing Movies"
@@ -21,5 +27,22 @@ public final class Constants {
         const val TV_TRENDING_NETFLIX_TITLE = "Trending TV Shows on Netflix"
         const val TV_TRENDING_HULU_TITLE = "Trending TV Shows on Hulu"
         const val TV_TRENDING_DISNEY_TITLE = "Trending TV Shows on Disney Plus"
+        // Favorite content constants
+        const val EMPTY_POSTER_PATH = ""
+        const val EMPTY_FIELD_STRING = ""
+        const val EMPTY_FIELD_INT = 0
+        const val FAVORITE_MOVIE_TYPE = "Movie"
+        const val FAVORITE_TV_SHOW_TYPE = "Tv Show"
+        const val DEFAULT_BINGE_STATUS = "Not Started Yet"
+        const val BINGING_BINGE_STATUS = "Binging"
+        const val SEEN_BINGE_STATUS = "Seen"
+        // FavoritePagerAdapter constants
+        const val MOVIE_TAB_TITLE = "Movies"
+        const val TV_SHOWS_TAB_TITLE = "TV Shows"
+        const val PAGER_PAGE_COUNT = 2
+        const val PAGER_MOVIE_TAB_POS = 0
+        // Binge status Alert Dialog constants
+        const val BINGE_STATUS_ALERT_DIALOG_TITLE = "Select Binge Status"
+        const val ALERT_DIALOG_CANCEL = "CANCEL"
     }
 }
