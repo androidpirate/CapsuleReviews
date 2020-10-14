1b756fff724b48540b8d991e30524207176fa53d
android_pirate <emrekalaycidesign@gmail.com>
2020-10-14 05:29:45 -0400

    Refactor - Update MovieGenresDialogFragment and layout to work with new GenreAdapter.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/dialog/MovieGenresDialogFragment.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/dialog/MovieGenresDialogFragment.kt
@@ -1,25 +1,23 @@
 package com.github.androidpirate.capsulereviews.ui.dialog
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
 import androidx.fragment.app.DialogFragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.androidpirate.capsulereviews.R
-import com.github.androidpirate.capsulereviews.ui.adapter.dialog.MovieGenresAdapter
+import com.github.androidpirate.capsulereviews.ui.adapter.dialog.GenreAdapter
+import com.github.androidpirate.capsulereviews.ui.adapter.dialog.GenreClickListener
 import com.github.androidpirate.capsulereviews.util.internal.Constants
 import com.github.androidpirate.capsulereviews.util.internal.GenreType
 import kotlinx.android.synthetic.main.movie_genres_dialog.*
 
 class MovieGenresDialogFragment(
     private val listener: MovieGenresDialogListener,
-    private val selectedPosition: Int) :
+    private val selectedPosition: Int):
     DialogFragment(),
-    MovieGenresAdapter.GenreClickListener {
+    GenreClickListener {
 
-    private lateinit var adapter: MovieGenresAdapter
+    private lateinit var adapter: GenreAdapter
 
     interface MovieGenresDialogListener {
         fun onGenreSelected(genre: GenreType)
@@ -27,7 +25,7 @@ class MovieGenresDialogFragment(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = MovieGenresAdapter(this, selectedPosition)
+        adapter = GenreAdapter(Constants.getMovieGenresArray(),this, selectedPosition)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -39,7 +37,6 @@ class MovieGenresDialogFragment(
         cancelButton.setOnClickListener {
             dialog?.cancel()
         }
-        movieGenres.layoutManager = LinearLayoutManager(requireContext())
         movieGenres.adapter = adapter
     }
 
@@ -49,11 +46,12 @@ class MovieGenresDialogFragment(
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.MATCH_PARENT
         )
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
     }
 
     private fun getSelectedGenre(itemPosition: Int): GenreType {
         val map = Constants.getMovieGenresMap()
-        return map[Constants.getMovieGenresArray()[itemPosition]] ?: error("No such genre in the array!")
+        return map[Constants.getMovieGenresArray()[itemPosition]] ?: error(Constants.GENRE_TYPE_ERROR_MESSAGE)
     }
 
     override fun onGenreClick(itemPosition: Int) {
