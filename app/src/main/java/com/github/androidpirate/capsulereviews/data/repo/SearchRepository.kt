cfca4c53ae2ed967217873288d036989059aebcb
android_pirate <emrekalaycidesign@gmail.com>
2020-10-19 11:31:42 -0400

    New feat - Implement SearchRepo to handle search data.

---
+++ app/src/main/java/com/github/androidpirate/capsulereviews/data/repo/SearchRepository.kt
@@ -0,0 +1,29 @@
+package com.github.androidpirate.capsulereviews.data.repo
+
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.github.androidpirate.capsulereviews.data.datasource.PagedSearchResultsDataSourceFactory
+import com.github.androidpirate.capsulereviews.data.network.api.MovieDbService
+import com.github.androidpirate.capsulereviews.data.network.response.multiSearch.NetworkMultiSearchListItem
+import com.github.androidpirate.capsulereviews.util.internal.Constants
+import kotlinx.coroutines.CoroutineScope
+
+class SearchRepository(
+    private val api: MovieDbService
+) {
+
+    fun getSearchResults(
+        scope: CoroutineScope,
+        queryString: String): LiveData<PagedList<NetworkMultiSearchListItem>> {
+        val searchDataSourceFactory = PagedSearchResultsDataSourceFactory(api, scope, queryString)
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(Constants.PAGE_SIZE)
+            .build()
+        return LivePagedListBuilder<Int, NetworkMultiSearchListItem>(
+            searchDataSourceFactory,
+            config)
+            .build()
+    }
+}
