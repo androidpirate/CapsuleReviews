8698d7966ea2ad68a5c052ceffe63311bd33e041
android_pirate <emrekalaycidesign@gmail.com>
2020-10-28 13:01:59 -0400

    Refactor - Update MoreFragment layout and links.

--- app/src/main/java/com/github/androidpirate/capsulereviews/ui/more/MoreFragment.kt
+++ app/src/main/java/com/github/androidpirate/capsulereviews/ui/more/MoreFragment.kt
@@ -1,18 +1,17 @@
 package com.github.androidpirate.capsulereviews.ui.more
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.androidpirate.capsulereviews.BuildConfig
 import com.github.androidpirate.capsulereviews.R
+import kotlinx.android.synthetic.main.fragment_more.*
 
 class MoreFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,4 +21,12 @@ class MoreFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_more, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tmdbLink.setOnClickListener {
+            val uri = Uri.parse(BuildConfig.MOVIE_DB_HOME_URL)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
 }
